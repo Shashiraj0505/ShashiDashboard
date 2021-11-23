40a0c572cc590f79483ade8214f7d2c2b538c5cd
Sharma <shashi.kumar.sharma@sap.com>
2021-11-23 16:07:40 +0530

    Controller added commit

---
+++ src/main/java/com/shashi/DashboardController.java
@@ -0,0 +1,15 @@
+package com.shashi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DashboardController {
+
+@GetMapping("/msg")
+	public String getMessages() {
+		
+		String msg="This is myTest of Jenkins";
+		return msg;
+	}
+}
